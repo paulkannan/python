102b37e56aaa7bd20638ff878c6e3f8da95ed8aa
paul kannan <paulkannan.25@gmail.com>
2021-07-08 12:13:34 +0530

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,25 @@
+def QuickSort(arr):
+    current_position = 0  # Position of the partitioning element
+
+    for i in range(1, len(arr)):  # Partitioning loop
+        if arr[i] <= arr[0]:
+            current_position += 1
+            temp = arr[i]
+            arr[i] = arr[current_position]
+            arr[current_position] = temp
+
+    temp = arr[0]
+    arr[0] = arr[current_position]
+    arr[current_position] = temp  # Brings pivot to it's appropriate position
+
+    left = QuickSort(arr[0:current_position])  # Sorts the elements to the left of pivot
+    right = QuickSort(arr[current_position + 1:len(arr)])  # sorts the elements to the right of pivot
+
+    arr = left + [arr[current_position]] + right  # Merging everything together
+
+    return arr
+
+
+array_to_be_sorted = [4, 2, 7, 3, 1, 6]
+print("Original Array: ", array_to_be_sorted)
+print("Sorted Array: ", QuickSort(array_to_be_sorted))
